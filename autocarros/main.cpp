af5ab3d8fd1d6698e665fb935eddb6c6b728119c
Rafael <up201910200@up.pt>
2022-05-25 17:28:14 +0100

    ford fulkerson a funcionar

--- autocarros/main.cpp
+++ autocarros/main.cpp
@@ -5,26 +5,25 @@
 #include "transporte.cpp"
 #include <list>
 #include <string>
+#include <queue>
 using std::cerr;
 using std::endl;
 int V;
 int veiculos;
-
+std::vector<transporte> vec;
 using namespace std;
+
+
 
 // A directed graph using adjacency list representation
 class Graph
 {
     int V;    // No. of vertices in graph
     list<int> *adj; // Pointer to an array containing adjacency lists
-
-    // A recursive function used by printAllPaths()
-    void printAllPathsUtil(int , int , bool [], int [], int &, ofstream& output);
-
 public:
     Graph(int V);   // Constructor
     void addEdge(int u, int v);
-    void printAllPaths(int s, int d, ofstream& output);
+
 };
 
 Graph::Graph(int V)
@@ -38,104 +37,146 @@ void Graph::addEdge(int u, int v)
     adj[u].push_back(v); // Add v to u’s list.
 }
 
-// Prints all paths from 's' to 'd'
-void Graph::printAllPaths(int s, int d, ofstream& output)
+bool BFS(vector< vector<int> > &resAdjMatrix, int &source, int &sink, vector<int> &parent)
 {
-    // Mark all the vertices as not visited
-    bool *visited = new bool[V];
+    // Create an array for all nodes we visited. Initialized to false.
+    int n = resAdjMatrix.size();
+    bool visited[n] = { false };
 
-    // Create an array to store paths
-    int *path = new int[V];
-    int path_index = 0; // Initialize path[] as empty
+    // Create a queue to check each node.
+    queue<int> q;
 
-    // Initialize all vertices as not visited
-    for (int i = 0; i < V; i++)
-        visited[i] = false;
+    // Push our source into the queue and mark it as visited. It has no parent.
+    q.push(source);
+    visited[source] = true;
+    parent[source] = -1;
 
-    // Call the recursive helper function to print all paths
-    printAllPathsUtil(s, d, visited, path, path_index, output);
-}
-
-// A recursive function to print all paths from 'u' to 'd'.
-// visited[] keeps track of vertices in current path.
-// path[] stores actual vertices and path_index is current
-// index in path[]
-void Graph::printAllPathsUtil(int u, int d, bool visited[],
-                              int path[], int &path_index, ofstream& output)
-{
-    // Mark the current node and store it in path[]
-    visited[u] = true;
-    path[path_index] = u;
-    path_index++;
-
-    // If current vertex is same as destination, then print
-    // current path[]
-    if (u == d)
+    // Keep visiting vertices.
+    while(q.empty() == false)
     {
-        for (int i = 0; i<path_index; i++){
-            cout << path[i] << " ";
-            output << path[i] << " ";
-        }
-        cout << endl;
-        output << endl;
-    }
-    else // If current vertex is not destination
-    {
-        // Recur for all the vertices adjacent to current vertex
-        list<int>::iterator i;
-        for (i = adj[u].begin(); i != adj[u].end(); ++i)
-            if (!visited[*i])
-                printAllPathsUtil(*i, d, visited, path, path_index, output);
-    }
+        int u = q.front();
+        q.pop();
 
-    // Remove current vertex from path[] and mark it as unvisited
-    path_index--;
-    visited[u] = false;
-}
-int maximizar_fluxo(std::vector<transporte> vec){
-    std::ifstream file("output.txt");
-    std::vector<transporte> final;
-    int x1, x2;
-    string line;
-    int j=2;
-    getline(file,line);
-    x1 = (int)line.at(0);
-    x2 = (int)line.at(2);
-    while(!file.eof()){
+        // Check all of u's friends.
+        for(int i = 0; i < n; i++)
+        {
+            int v = i;
+            int capacity = resAdjMatrix[u][v];
 
-        for ( std::string::iterator it=line.begin(); it!=line.end(); ++it){
-            for(int i=0; i<veiculos; i++){
-                if(vec.at(i).origem == x1 && vec.at(i).destino == x2){
-                    final.push_back(transporte(x1,x2,vec.at(i).capacidade,0,i));
-                }
+            // We find a neighbor that hasn't been visited, and the capacity is bigger than 0.
+            if(visited[v] == false && capacity > 0)
+            {
+                // Push the neighbor onto the queue, mark it's parent, and mark it as visited.
+                q.push(v);
+                parent[v] = u;
+                visited[v] = true;
             }
         }
-        getline(file,line);
     }
+
+    // If the sink got visited, then we found a path to it.
+    if(visited[sink] == true)
+    { return true; }
+
+    return false;
 }
-// Driver program
+
+int FordFulkerson(vector< vector<int> > &adjMatrix, int &source, int &sink)
+{
+    int maxflow = 0;
+
+    // 1. Create the residual graph. (Same as the original graph.)
+    vector< vector<int> > resAdjMatrix;
+    int n = adjMatrix.size();
+    for(int i = 0; i < n; i++)
+    {
+        vector<int> row;
+        resAdjMatrix.push_back(row);
+        for(int j = 0; j < adjMatrix[i].size(); j++)
+        {
+            resAdjMatrix[i].push_back(adjMatrix[i][j]);
+        }
+    }
+
+    // 2. Create an empty parent array for BFS to store the augmenting path.
+    vector<int> parent;
+    for(int i = 0; i < n; i++)
+    {
+        parent.push_back(-1);
+    }
+
+    // 3. Keep calling BFS to check for an augmenting path (from the source to the sink...
+    while(BFS(resAdjMatrix, source, sink, parent) == true)
+    {
+        // 4. Find the max flow through the path we just found.
+        int pathflow = 10000007;
+
+        // Go through the path we just found. Iterate through the path.
+        int v = sink;
+        while(v != source)
+        {
+            int u = parent[v]; // The parent.
+
+            // Update the pathflow to this capacity if it's smaller
+            int capacity = resAdjMatrix[u][v];
+            pathflow = min(pathflow, capacity);
+
+            // Setup for the next edge in the path.
+            v = u;
+        }
+
+        // 5. Update the residual capacities of the edges and reverse edges.
+        v = sink;
+        while(v != source)
+        {
+            int u = parent[v]; // The parent.
+
+            // Update the capacities.
+
+            resAdjMatrix[u][v] -= pathflow;
+            resAdjMatrix[v][u] += pathflow;
+
+            // Setup for the next edge in the path.
+            v = u;
+        }
+
+        // 6. Add this path's flow to our total max flow so far.
+        maxflow += pathflow;
+    }
+
+    return maxflow;
+}
+
 int main()
 {
+    vector<vector<int>> adjMatrix;
     std::ofstream output;
     output.open("output.txt");
     int x1, x2, x3, x4;
-    std::vector<transporte> vec;
-    std::ifstream newfile("input_simples.txt");
+    std::ifstream newfile("in01.txt");
     newfile >> x1 >> x2;
     V = x1;
     veiculos = x2;
+    for(int i = 0; i < V; i++)
+    {
+        vector<int> row;
+        adjMatrix.push_back(row);
+        for(int j = 0; j < V; j++)
+        {
+            adjMatrix[i].push_back(0);
+        }
+    }
     Graph g(V);
     for(int i=0; i<veiculos; i++){
         newfile >> x1 >> x2 >> x3 >> x4;
         vec.push_back(transporte(x1, x2, x3, x4, i));
         g.addEdge(x1, x2);
+        adjMatrix[x1][x2]=x3;
     }
     newfile.close();
-    int s = 1, d = 5;
-    cout << "Following are all different paths from " << s
-         << " to " << d << endl;
-    g.printAllPaths(s, d, output);
+    int source = 20, sink = 7;
     output.close();
-    maximizar_fluxo(vec);
+    cout << "The max flow from " << source << " to " << sink << " is: ";
+    cout << FordFulkerson(adjMatrix, source, sink) << endl;
     return 0;
 }
