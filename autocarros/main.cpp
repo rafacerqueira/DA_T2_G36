e3572d7c2b8fd0211957a996ecd7d15c3bafbe0c
Rafael <up201910200@up.pt>
2022-06-01 17:14:59 +0100

    quase cenario1

--- autocarros/main.cpp
+++ autocarros/main.cpp
@@ -1,200 +1,3 @@
-/*#include <iostream>
-#include <vector>
-#include <fstream>
-#include <cstdlib>
-#include "transporte.cpp"
-#include <list>
-#include <string>
-#include <queue>
-using std::cerr;
-using std::endl;
-int V;
-int veiculos;
-std::vector<transporte> vec;
-using namespace std;
-
-
-
-// A directed graph using adjacency list representation
-class Graph
-{
-    int V;    // No. of vertices in graph
-    list<int> *adj; // Pointer to an array containing adjacency lists
-public:
-    Graph(int V);   // Constructor
-    void addEdge(int u, int v);
-
-};
-
-Graph::Graph(int V)
-{
-    this->V = V;
-    adj = new list<int>[V];
-}
-
-void Graph::addEdge(int u, int v)
-{
-    adj[u].push_back(v); // Add v to u’s list.
-}
-
-bool BFS(vector< vector<int> > &resAdjMatrix, int &source, int &sink, vector<int> &parent)
-{
-    // Create an array for all nodes we visited. Initialized to false.
-    int n = resAdjMatrix.size();
-    bool visited[n] = { false };
-
-    // Create a queue to check each node.
-    queue<int> q;
-
-    // Push our source into the queue and mark it as visited. It has no parent.
-    q.push(source);
-    visited[source] = true;
-    parent[source] = -1;
-
-    // Keep visiting vertices.
-    while(q.empty() == false)
-    {
-        int u = q.front();
-        q.pop();
-
-        // Check all of u's friends.
-        for(int i = 0; i < n; i++)
-        {
-            int v = i;
-            int capacity = resAdjMatrix[u][v];
-
-            // We find a neighbor that hasn't been visited, and the capacity is bigger than 0.
-            if(visited[v] == false && capacity > 0)
-            {
-                // Push the neighbor onto the queue, mark it's parent, and mark it as visited.
-                q.push(v);
-                parent[v] = u;
-                visited[v] = true;
-            }
-        }
-    }
-
-    // If the sink got visited, then we found a path to it.
-    if(visited[sink] == true)
-    { return true; }
-
-    return false;
-}
-
-int FordFulkerson(vector< vector<int> > &adjMatrix, int &source, int &sink)
-{
-    int maxflow = 0;
-
-    // 1. Create the residual graph. (Same as the original graph.)
-    vector< vector<int> > resAdjMatrix;
-    int n = adjMatrix.size();
-    for(int i = 0; i < n; i++)
-    {
-        vector<int> row;
-        resAdjMatrix.push_back(row);
-        for(int j = 0; j < adjMatrix[i].size(); j++)
-        {
-            resAdjMatrix[i].push_back(adjMatrix[i][j]);
-        }
-    }
-
-    // 2. Create an empty parent array for BFS to store the augmenting path.
-    vector<int> parent;
-    for(int i = 0; i < n; i++)
-    {
-        parent.push_back(-1);
-    }
-
-    // 3. Keep calling BFS to check for an augmenting path (from the source to the sink...
-    while(BFS(resAdjMatrix, source, sink, parent) == true)
-    {
-        // 4. Find the max flow through the path we just found.
-        int pathflow = 10000007;
-
-        // Go through the path we just found. Iterate through the path.
-        int v = sink;
-        while(v != source)
-        {
-            int u = parent[v]; // The parent.
-
-            // Update the pathflow to this capacity if it's smaller
-            int capacity = resAdjMatrix[u][v];
-            pathflow = min(pathflow, capacity);
-
-            // Setup for the next edge in the path.
-            v = u;
-        }
-
-        // 5. Update the residual capacities of the edges and reverse edges.
-        v = sink;
-        while(v != source)
-        {
-            int u = parent[v]; // The parent.
-
-            // Update the capacities.
-
-            resAdjMatrix[u][v] -= pathflow;
-            resAdjMatrix[v][u] += pathflow;
-
-            // Setup for the next edge in the path.
-            v = u;
-        }
-
-        // 6. Add this path's flow to our total max flow so far.
-        maxflow += pathflow;
-    }
-
-    return maxflow;
-}
-
-int main()
-{
-    vector<vector<int>> adjMatrix;
-    std::ofstream output;
-    output.open("output.txt");
-    int x1, x2, x3, x4;
-    std::ifstream newfile("input_simples.txt");
-    newfile >> x1 >> x2;
-    V = x1;
-    veiculos = x2;
-    for(int i = 0; i < V; i++)
-    {
-        vector<int> row;
-        adjMatrix.push_back(row);
-        for(int j = 0; j < V; j++)
-        {
-            adjMatrix[i].push_back(0);
-        }
-    }
-    Graph g(V);
-    for(int i=0; i<veiculos; i++){
-        newfile >> x1 >> x2 >> x3 >> x4;
-        vec.push_back(transporte(x1, x2, x3, x4, i));
-        g.addEdge(x1, x2);
-        adjMatrix[x1][x2]=x3;
-    }
-    newfile.close();
-    int source=2, sink=3;
-    int max_flow = 0;
-    int flow;
-    for(int i=1; i<=5; i++){
-        for(int j=i;j<=5; j++) {
-            int s = i, d = j;
-            cout << "entrou" << endl;
-            flow = FordFulkerson(adjMatrix, source, sink);
-            cout << "saiu" << endl;
-            if (flow >= max_flow) {
-                max_flow = flow;
-                source = s;
-                sink = d;
-            }
-        }
-    }
-    cout << "The max flow from " << source << " to " << sink << " is: ";
-    cout << max_flow << endl;
-    output.close();
-    return 0;
-}*/
 #include<cstdio>
 #include<queue>
 #include<cstring>
@@ -202,27 +5,29 @@ int main()
 #include<iostream>
 #include <fstream>
 using namespace std;
-int c[10][10];
-int flowPassed[10][10];
-vector<int> g[10];
-int parList[10];
-int currentPathC[10];
+int c[5000][5000];
+int flowPassed[5000][5000];
+vector<int> g[5000];
+int parList[5000];
+int currentPathC[5000];
 #include <list>
+#include <iostream>
+#include <list>
+#include <queue>
+#define INFINITO 10000000
 
-
-// A directed graph using
-// adjacency list representation
+using namespace std;
 class Graph {
     int V; // No. of vertices in graph
     list<int>* adj; // Pointer to an array containing adjacency lists
 
     // A recursive function used by printAllPaths()
-    void printAllPathsUtil(int, int, bool[], int[], int&);
+    void printAllPathsUtil(int, int, bool[], int[], int&, ofstream&);
 
 public:
     Graph(int V); // Constructor
     void addEdge(int u, int v);
-    void printAllPaths(int s, int d);
+    void printAllPaths(int s, int d, ofstream& output);
 };
 
 Graph::Graph(int V)
@@ -237,13 +42,13 @@ void Graph::addEdge(int u, int v)
 }
 
 // Prints all paths from 's' to 'd'
-void Graph::printAllPaths(int s, int d)
+void Graph::printAllPaths(int s, int d, ofstream& output)
 {
     // Mark all the vertices as not visited
-    bool *visited = new bool[V];
+    bool* visited = new bool[V];
 
     // Create an array to store paths
-    int *path = new int[V];
+    int* path = new int[V];
     int path_index = 0; // Initialize path[] as empty
 
     // Initialize all vertices as not visited
@@ -251,7 +56,7 @@ void Graph::printAllPaths(int s, int d)
         visited[i] = false;
 
     // Call the recursive helper function to print all paths
-    printAllPathsUtil(s, d, visited, path, path_index);
+    printAllPathsUtil(s, d, visited, path, path_index, output);
 }
 
 // A recursive function to print all paths from 'u' to 'd'.
@@ -259,7 +64,7 @@ void Graph::printAllPaths(int s, int d)
 // path[] stores actual vertices and path_index is current
 // index in path[]
 void Graph::printAllPathsUtil(int u, int d, bool visited[],
-                              int path[], int &path_index)
+                              int path[], int& path_index, ofstream& output)
 {
     // Mark the current node and store it in path[]
     visited[u] = true;
@@ -268,12 +73,10 @@ void Graph::printAllPathsUtil(int u, int d, bool visited[],
 
     // If current vertex is same as destination, then print
     // current path[]
-    if (u == d)
-    {
-        for (int i = 0; i<path_index; i++){
-            cout << path[i] << " ";
-        }
-        cout << endl;
+    if (u == d) {
+        for (int i = 0; i < path_index; i++)
+            output << path[i] << " ";
+        output << endl;
     }
     else // If current vertex is not destination
     {
@@ -281,13 +84,16 @@ void Graph::printAllPathsUtil(int u, int d, bool visited[],
         list<int>::iterator i;
         for (i = adj[u].begin(); i != adj[u].end(); ++i)
             if (!visited[*i])
-                printAllPathsUtil(*i, d, visited, path, path_index);
+                printAllPathsUtil(*i, d, visited, path, path_index, output);
     }
 
     // Remove current vertex from path[] and mark it as unvisited
     path_index--;
     visited[u] = false;
 }
+// A directed graph using
+// adjacency list representation
+
 int bfs(int sNode, int eNode)//breadth first search
 {
     memset(parList, -1, sizeof(parList));
@@ -321,6 +127,33 @@ int bfs(int sNode, int eNode)//breadth first search
     }
     return 0;
 }
+void read(ifstream& file){
+    std::string line;
+    std::getline(file, line);
+    int length = line.length();
+    int arr[length]= { 0 };
+    int j = 0, i, sum = 0;
+
+    // Traverse the string
+    for (i = 0; line[i] != '\0'; i++) {
+        if (line[i] == ' '){
+            // Increment j to point to next
+            // array location
+            j++;
+        }
+        else {
+
+            // subtract str[i] by 48 to convert it to int
+            // Generate number by multiplying 10 and adding
+            // (int)(str[i])
+            arr[j] = arr[j] * 10 + (line[i] - 48);
+        }
+    }
+    for (i = 0; i < j; i++) {
+        cout << arr[i] << " ";
+    }
+
+}
 int edmondsKarp(int sNode, int eNode)
 {
     int maxFlow = 0;
@@ -336,6 +169,7 @@ int edmondsKarp(int sNode, int eNode)
         while(currNode != sNode)
         {
             int prevNode = parList[currNode];
+
             flowPassed[prevNode][currNode] += flow;
             flowPassed[currNode][prevNode] -= flow;
             currNode = prevNode;
@@ -343,9 +177,7 @@ int edmondsKarp(int sNode, int eNode)
     }
     return maxFlow;
 }
-int main()
-{
-
+void cenario2_3(){
     std::ofstream output;
     output.open("output.txt");
     int nodCount, edCount;
@@ -370,6 +202,94 @@ int main()
         graph.addEdge(x1,x2);
     }
     int maxFlow = edmondsKarp(source, sink);
-    cout<<endl<<endl<<"Max Flow is:"<<maxFlow<<endl;
-    graph.printAllPaths(source, sink);
+    cout<<"Max Flow is:"<<maxFlow<<endl;
+}
+void cenario1_1() {
+    std::ofstream output;
+    //output.open("output.txt");
+    int nodCount, edCount;
+    int x1, x2, x3, x4;
+    std::ifstream newfile("input_simples.txt");
+    newfile >> x1 >> x2;
+    nodCount=x1;
+    edCount=x2;
+    int source, sink;
+    source=1;
+    sink=x1;
+    Graph g(x1);
+    for(int ed = 0; ed < edCount; ed++){
+        newfile >> x1 >> x2 >> x3 >> x4;
+        g.addEdge(x1,x2);
+        c[x1][x2]=x3;
+    }
+    newfile.close();
+    //g.printAllPaths(1,5, output);
+    //output.close();
+    std::ifstream file("output.txt");
+    read(file);
+}
+void cenario2_1(){
+    std::ofstream output;
+    output.open("output.txt");
+    int dimensao;
+    cout << "Que dimensao pretende?" << endl;
+    cin >> dimensao;
+    cout << "dimensao de: " << dimensao << endl;
+    int nodCount, edCount;
+    int x1, x2, x3, x4;
+    std::ifstream newfile("in01_b.txt");
+    newfile >> x1 >> x2;
+    nodCount=x1;
+    edCount=x2;
+    Graph graph(nodCount);
+    int source, sink;
+    source=0;
+    sink=x1+1;
+    c[0][1] = dimensao;
+    g[0].push_back(1);
+    c[x1][x1+1] = dimensao;
+    g[x1].push_back(x1+1);
+
+    //50<-12<-46<-8<-1<-50<-41<-33<-6<-1<-50<-31<-40<-48<-8<-1<-50<-39<-19<-7<-37<-38<-1<-50<-39<-44<-22<-35<-38<-1<-50<-39<-17<-36<-10<-6<-1<-
+
+    for(int ed = 0; ed < edCount; ed++){
+        graph.addEdge(0,x1+1);
+        newfile >> x1 >> x2 >> x3 >> x4;
+        int from, to, cap;
+        from=x1;
+        to=x2;
+        cap=x3;
+        c[from][to] = cap;
+        g[from].push_back(to);
+        g[to].push_back(from);
+        graph.addEdge(x1,x2);
+    }
+    edmondsKarp(source,sink);
+
+}
+void displaymenu(){ cout<<"===================================================== \n";
+cout<<" \t\tMENU \t \n ";
+cout<<"===================================================== \n";
+cout<<" 1.Determinar a dimensão máxima do grupo e um encaminhamento.\n";
+cout<<" 2.Maximizar a dimensão do grupo e minimizar o número de transbordos, sem privilegiar um dos critérios relativamente ao outro.\n";
+cout<<" 3.Determinar um encaminhamento para um grupo, dada a sua dimensão.\n";
+cout<<" 4.View all student records \n";
+cout<<" 5.Find a student by ID \n";
+}
+
+
+int main(){
+    displaymenu();
+    int yourchoice;
+     cout<<"Enter your choice(1-5):";
+        cin>>yourchoice;
+        switch (yourchoice)
+        {
+            case 1: cenario2_3(); break;
+            case 2: cenario1_1();break;
+            case 3: cenario2_1(); break;
+            default: cout<<"invalid"; break;
+        }
+
+    return 0;
 }
